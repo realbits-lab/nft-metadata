80b5ba815c600d44ba9b756c1a72aaa16d825b30
realbits-lab <dev.realbits@gmail.com>
2023-01-31 17:53:56 +0900

    Update body_bottom and top file.

--- scripts/clothes_metadata.py
+++ scripts/clothes_metadata.py
@@ -74,9 +74,9 @@ list_body = [
 #                  "8", "9", "10", "11", "12", "13", "14", "15", ]
 list_body_top = ["1", ]
 
-list_body_bottom = ["1", "2", "3", "4", "5", "6", "7",
-                    "8", "9", "10", "11", "12", "13", "14", "15", ]
-# list_body_bottom = ["1", ]
+# list_body_bottom = ["1", "2", "3", "4", "5", "6", "7",
+#                     "8", "9", "10", "11", "12", "13", "14", "15", ]
+list_body_bottom = ["2", ]
 
 metadata.generate_metadata(
     output_dir, nft_name, nft_symbol, nft_description, nft_image_url,
